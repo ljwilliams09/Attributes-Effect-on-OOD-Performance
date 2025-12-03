1ba16497b8ab6784fcb89c969666390e2c7f67b8
Matthew Liu <zliu1@colgate.edu>
2025-12-03 13:21:21 -0500

    add file name to csv

--- image_generators/ImageGeneration.py
+++ image_generators/ImageGeneration.py
@@ -1,11 +1,14 @@
 from openai import OpenAI
 import csv
 import os
+from dotenv import load_dotenv
 import base64
+
+load_dotenv()
 
 client = OpenAI()
 
-csv_file_path = 'prompts.csv'
+csv_file_path = './Tasks/stratification/prompt_stratification.csv'
 output_directory = 'generated_images'
 
 os.makedirs(output_directory, exist_ok=True)
@@ -14,8 +17,9 @@ with open(csv_file_path, mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     
     for i, row in enumerate(reader):
+        
         prompt = row['prompt']
-
+        file_name = f"wine_{i}.png"
         try:
             response = client.images.generate(
                 model="gpt-image-1",
@@ -24,14 +28,16 @@ with open(csv_file_path, mode='r', encoding='utf-8') as file:
             )
             if not response.data or not response.data[0].b64_json:
                 print(f"[{i}] ERROR: No image returned.")
+                row['file_name'] = ""
                 continue
             image_base64 = response.data[0].b64_json
             image_bytes = base64.b64decode(image_base64)
 
-            image_path = os.path.join(output_directory, f"image_{i}.png")
+            image_path = os.path.join(output_directory, f"wine_{i}.png")
             with open(image_path, "wb") as f:
                 f.write(image_bytes)
 
+            row['file_name'] = file_name
             print(f"[{i}] Saved: {image_path} | Prompt: {prompt[:50]}...")
 
         except Exception as e:
