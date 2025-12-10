09f7b8a73d5ea1177c0fb4a8057950266608959f
Luca Williams <ljwilliams@colgate.edu>
2025-12-10 11:06:25 -0500

    moved some stuff

--- analysis/main.R
+++ analysis/main.R
@@ -8,7 +8,7 @@ library(MASS)
 library(corrplot)
 
 # ---- Read dataset ----
-df <- read.csv("./../stratification/wine_prompts.csv", stringsAsFactors = FALSE)
+df <- read.csv("./stratification/wine_prompts.csv", stringsAsFactors = FALSE)
 
 
 # ---- Clean + preprocess ----
@@ -27,14 +27,14 @@ numeric_vars <- c(
 
 df[numeric_vars] <- scale(df[numeric_vars])
 
-# ---- Correlation check ----
-cor_mat <- cor(df[numeric_vars])
-corrplot(
-  cor_mat,
-  method = "color",
-  addCoef.col = "black",
-  tl.cex = 0.8
-)
+# # ---- Correlation check ----
+# cor_mat <- cor(df[numeric_vars])
+# corrplot(
+#   cor_mat,
+#   method = "color",
+#   addCoef.col = "black",
+#   tl.cex = 0.8
+# )
 
 # ---- Linear Regression ---- Using robust regression
 lm_model <- lm(
@@ -66,3 +66,12 @@ robust_model <- rlm(
 )
 
 summary(robust_model)
+
+cat("\n---- OLS SUMMARY ----\n")
+print(summary(lm_model))
+
+cat("\n---- VIF ----\n")
+print(vif(lm_model))
+
+cat("\n---- ROBUST REGRESSION SUMMARY ----\n")
+print(summary(robust_model))
