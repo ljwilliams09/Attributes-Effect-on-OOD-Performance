d61ed16b86dedf53b21799a8d70e796a488cf913
Luca Williams <ljwilliams@colgate.edu>
2025-12-10 12:07:55 -0500

    Refactor code structure for improved readability and maintainability

--- stratification/prompt_stratification.py
+++ stratification/prompt_stratification.py
@@ -2,15 +2,15 @@ import csv
 import gpt_prompter
 
 def main():
-    base_prompt = "a glass of red wine"
-    system_prompt = "You are a research assistant that is creating prompts for us to test on image generation. You will be given a list of prompt attributes, and a target object for the image generator to create. Return one image prompt that follows the attribute guidelines to elicit the photo from the image generator. Return only the final image prompt with no explanation or extra text. Your main focus should be getting a glass of red wine." \
+    base_prompt = "an empty stick of deodorant"
+    system_prompt = "You are a research assistant that is creating prompts for us to test on image generation. You will be given a list of prompt attributes, and a target object for the image generator to create. Return one image prompt that follows the attribute guidelines to elicit the photo from the image generator. Return only the final image prompt with no explanation or extra text. Your main focus should be getting the deodorant stick to be empty and have no deodorant in it." \
     "Here are descriptions of what each variable means: " \
     "descriptor_words_count: adjectives + adverbs count" \
     "spatial_constraints: how the object is viewed and in what perspective is it portrayed" \
     "num_visual_attributes: (color words, size words)" \
     "sentence_count: number of sentences" \
     "style: photorealistic" \
-    "ambiguity: how ambiguous is the prompt? clear would mean that it is directly indicated that it is a wine glass, ambiguous would mean that the object is indirectly referenced."
+    "ambiguity: how ambiguous is the prompt? clear would mean that it is directly indicated that it is a deodorant stick, ambiguous would mean that the object is indirectly referenced."
 
     id = 0
     descriptor_words = 3
@@ -31,7 +31,7 @@ def main():
                             id += 1
                             combinations.append([id, None, des_words, sen_con, visual, constraint, amb_score])
     
-    with open("./wine_control.csv", 'w') as f:
+    with open("./deodorant.csv", 'w') as f:
         writer = csv.writer(f)
         writer.writerow(headers)
         for row in combinations:
