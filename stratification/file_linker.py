84315a058ef920cf5ae88f984df9331718f9243d
Luca Williams <ljwilliams@colgate.edu>
2025-12-11 10:23:13 -0500

    images

--- stratification/file_linker.py
+++ stratification/file_linker.py
@@ -1,14 +1,14 @@
 import pandas as pd
 count = 0
-df = pd.read_csv("./wine_control_prompts.csv")
+df = pd.read_csv("./prompts/deodorant_prompts.csv")
 
 def generate_filename(row):
     global count
-    filename = f"wine_control_{count}.png"
+    filename = f"deodorant_{count}.png"
     count += 1
     return filename
 
 df['filename'] = df.apply(generate_filename, axis=1)
 
 # Write the updated DataFrame back to the original CSV
-df.to_csv("./wine_control_prompts.csv", index=False)
+df.to_csv("./prompts/deodorant_prompts.csv", index=False)
