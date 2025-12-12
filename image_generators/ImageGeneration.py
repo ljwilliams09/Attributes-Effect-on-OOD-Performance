ae6a50bb04a9caeb40afee96c757394ef88cdb20
Matthew Liu <zliu1@colgate.edu>
2025-12-12 13:37:16 -0500

    rubikpython

--- image_generators/ImageGeneration.py
+++ image_generators/ImageGeneration.py
@@ -33,7 +33,7 @@ with open(csv_file_path, mode='r', encoding='utf-8') as file:
             image_base64 = response.data[0].b64_json
             image_bytes = base64.b64decode(image_base64)
 
-            image_path = os.path.join(output_directory, f"rubik_{i}.png")
+            image_path = os.path.join(output_directory, f"rubik_{i+48}.png")
             with open(image_path, "wb") as f:
                 f.write(image_bytes)
 
