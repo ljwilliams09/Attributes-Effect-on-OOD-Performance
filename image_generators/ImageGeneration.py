d498e3b2506e59558a04c9d93b2a7fa081c950d3
Matthew Liu <zliu1@colgate.edu>
2025-12-15 10:58:44 -0500

    image generation cmd line

--- image_generators/ImageGeneration.py
+++ image_generators/ImageGeneration.py
@@ -14,8 +14,6 @@ from dotenv import load_dotenv
 import base64
 import argparse
 
-<<<<<<< HEAD
-
 def main(args):
     # load OPENAI_API_KEY from environment
     load_dotenv()        
@@ -101,57 +99,3 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(args)
-=======
-load_dotenv()
-
-client = OpenAI()
-
-
-def generator(input_csv, output_directory, api_key):
-    load_dotenv()
-
-    client = OpenAI(api_key=api_key)
-
-    os.makedirs(output_directory, exist_ok=True)
-
-    with open(input_csv, mode='r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        
-        for i, row in enumerate(reader):
-            
-            prompt = row['prompt']
-            file_name = f"wine_{i}.png"
-            try:
-                response = client.images.generate(
-                    model="gpt-image-1",
-                    prompt=prompt,
-                    size="1024x1024"
-                )
-                if not response.data or not response.data[0].b64_json:
-                    print(f"[{i}] ERROR: No image returned.")
-                    row['file_name'] = ""
-                    continue
-                image_base64 = response.data[0].b64_json
-                image_bytes = base64.b64decode(image_base64)
-
-                image_path = os.path.join(output_directory, f"rubik_{i+48}.png")
-                with open(image_path, "wb") as f:
-                    f.write(image_bytes)
-
-                row['file_name'] = file_name
-                print(f"[{i}] Saved: {image_path} | Prompt: {prompt[:50]}...")
-
-            except Exception as e:
-                print(f"Error generating image for '{prompt}': {e}")
-
-def main():
-    parser = argparse.ArgumentParser(description="Image Generation Script")
-    parser.add_argument("--prompt_file", type=str, help="Path to csv file of prompts to be generated")
-    parser.add_argument("--output_dir", type=str, default="./", help="Directory for the images to be output to")
-    parser.add_argument("--openai_key", type=str, required=True, help="OpenAI API key for image generation")
-    args = parser.parse_args()
-    generator(args.prompt_file, args.output_dir, args.api_key)
-
-if __name__ == "__main__":
-    main()
->>>>>>> 2c2c6973b907c9886f297bfc609bd047738f9241
