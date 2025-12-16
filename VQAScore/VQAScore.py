52c155780d4e821f53538288917dd9758906dabe
Luca Williams <ljwilliams@colgate.edu>
2025-12-15 19:11:18 -0500

    final push, December 15th, 2025

--- VQAScore/VQAScore.py
+++ VQAScore/VQAScore.py
@@ -1,5 +1,5 @@
 import t2v_metrics
 
 def vqa_score(image_files, key, base_prompt):
-    score_func = t2v_metrics.get_score_model(model="gpt-4.1", device="cuda", api_key=key, top_logprobs=20) 
+    score_func = t2v_metrics.get_score_model(model="gpt-4.1", device="cuda", api_key=key, top_logprobs=20) # type: ignore
     return score_func(images=image_files, texts=base_prompt).flatten().cpu().tolist()
